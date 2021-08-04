d3525137527c65a776fca87a8ab8c2443b9edae9
melamotome <43264267+melamotome@users.noreply.github.com>
2021-08-04 23:47:02 +0300

    Initial commit

---
+++ src/introduction/FibonacciSmall.java
@@ -0,0 +1,8 @@
+package introduction;
+//Дано целое число 1≤n≤40, необходимо вычислить n-е число Фибоначчи
+// (напомним, что F0=0, F1=1, Fn=Fn−1+Fn−2 при n≥2)
+public class FibonacciSmall {
+    public static void main(String[] args) {
+
+    }
+}
